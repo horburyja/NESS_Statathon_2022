7fa45add112813f11d583f44ad2fcf62efc6207b
James Horbury <james.horbury@uconn.edu>
2022-05-04 10:20:24 -0400

    Added descriptive stats

--- data_exploration.py
+++ data_exploration.py
@@ -7,13 +7,14 @@ import numpy as np
 train_raw = pd.read_csv('data/train.csv')
 test_raw = pd.read_csv('data/test.csv')
 print("\nRaw training df of shape {}\n".format(train_raw.shape))
+print(train_raw.info(), "\n")
 
 # missing data percentage list
 print("Missing training data:")
 
 types = train_raw.dtypes
 
-total_pct_missing, colnum = 0, 1
+total_pct_missing, colnum = 0, 0
 for col in train_raw.columns:
     mean_missing = np.mean(train_raw[col].isnull())
     pct_missing = round(mean_missing * 100, 5) # round to 5 decimal places
@@ -24,17 +25,19 @@ for col in train_raw.columns:
 
 print('\n{}% missing in total\n'.format(total_pct_missing))
 
-train_raw_numeric = train_raw.select_dtypes(include=[np.number])
-numeric_cols = train_raw_numeric.columns.values
-print("Numeric cols [{}]: {}\n".format(len(numeric_cols), numeric_cols))
-
 train_raw_non_numeric = train_raw.select_dtypes(exclude=[np.number])
 non_numeric_cols = train_raw_non_numeric.columns.values
 print("Non numeric cols [{}]: {}\n".format(len(non_numeric_cols),non_numeric_cols))
 
-# generate histograms
+train_raw_numeric = train_raw.select_dtypes(include=[np.number])
+numeric_cols = train_raw_numeric.columns.values
+print("Numeric cols [{}]: {}\n".format(len(numeric_cols), numeric_cols))
+
+# generate histograms and display descriptive stats
+print("--DISPLAY DESCRIPTIVE STATISTICS--")
 for col in numeric_cols:
     hist = train_raw[[col]].hist(bins=100) # keep number of bins at 100
     plt.title("{}_hist".format(col))
-    plt.savefig('figs/{}_hist.png'.format(col))
+    plt.savefig('figs/histograms/{}_hist.png'.format(col))
 
+    print("{} descriptive statistics:\n{}\n".format(col, train_raw[col].describe()))
